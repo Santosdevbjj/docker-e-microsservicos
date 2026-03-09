aad8ac45020002d204d3119c1432d4cc238f26f7
Sergio <santossergiorealbjj@outlook.com>
2026-03-09 17:15:37 -0300

    Create Payment.java

---
+++ src/main/java/com/faang/payments/model/Payment.java
@@ -0,0 +1,17 @@
+package com.faang.payments.model;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Payment {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private Long userId;
+    private Long productId;
+    private Double amount;
+}
