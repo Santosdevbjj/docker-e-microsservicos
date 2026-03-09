8537647d3b34565af51f75c1f762157b97748959
Sergio <santossergiorealbjj@outlook.com>
2026-03-09 17:11:01 -0300

    Create Product.java

---
+++ src/main/java/com/faang/products/model/Product.java
@@ -0,0 +1,16 @@
+package com.faang.products.model;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Product {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private Double price;
+}
